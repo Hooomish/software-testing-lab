9270a63f6944d8877f6f4d9409aac447302a1dd6
Nastassia <coca-cola23-3@mail.ru>
2018-09-14 22:54:43 +0300

    Commit 8

--- Laba 1/Laba 1/Student.cs
+++ Laba 1/Laba 1/Student.cs
@@ -8,9 +8,9 @@ namespace Laba_1
 {
     public class Student
     {
-        private List<int> Marks; //comment 4
+        private List<int> Marks; //comment 4.1
 
-        public string Name { get; set; } //comment 5
+        public string Name { get; set; } //comment 5.1
 
         public Student(string Name) //comment 3
         {
