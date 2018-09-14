c1a4343d36326a1040a37f1de98798bd0b564f31
Nastassia <coca-cola23-3@mail.ru>
2018-09-14 22:58:39 +0300

    Commit 10

--- Laba 1/Laba 1/Student.cs
+++ Laba 1/Laba 1/Student.cs
@@ -18,7 +18,7 @@ namespace Laba_1
             this.Marks = new List<int>();
         }
 
-        public void AddMark(int mark) //comment 2
+        public void AddMark(int mark) //comment 2.1
         {
             if (mark >= 0 && mark <= 10)
                 this.Marks.Add(mark);
