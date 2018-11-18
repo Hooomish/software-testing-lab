9e6c0f177dbc04d914e8f277ef94de957b353db9
Anastasia <coca-cola23-3@mail.ru>
2018-11-19 02:06:34 +0300

    PageObject

---
+++ PageObject/PageObject/Aviaru.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject
+{
+    [TestClass]
+    class Aviaru
+    {
+        IWebDriver driver;
+
+        public Aviaru(IWebDriver webDriver)
+        {
+            driver = webDriver;
+            driver.Manage().Window.Maximize();
+            PageFactory.InitElements(driver, this);
+        }
+
+        bool ClickElement()
+        {
+            try
+            {
+                driver.Url = "https://www.aviaru.de/ru/";
+
+                var urlForPanelElement = driver.FindElement(By.Name("ypsnet-ibe-iframe"));
+
+                driver.Url = urlForPanelElement.GetAttribute("src");
+
+                driver.FindElement(By.XPath(@"//*[@id='depapt1_name']")).SendKeys("Минск");
+                driver.FindElement(By.XPath(@"//*[@id='dstapt1_name']")).SendKeys("Москва");
+                driver.FindElement(By.XPath("//*[@id='depdate_add_day']/span")).Click();
+                driver.FindElement(By.XPath(@"//*[@id='search_btn']")).Click();
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public void ValidateReult()
+        {
+            Assert.AreEqual(true, ClickElement());
+        }
+    }
+}
---
+++ PageObject/PageObject/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PageObject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver Driver;
+
+        [TestInitialize]
+        public void TestMethod()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void SetupTest()
+        {
+            Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTicketWithoutAgreement()
+        {
+            Aviaru aviaru = new Aviaru(Driver);
+            aviaru.ValidateReult();
+        }
+    }
+}
