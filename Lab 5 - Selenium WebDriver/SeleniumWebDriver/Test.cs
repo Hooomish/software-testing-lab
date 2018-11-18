a7ce20742f684caca2b88ab895d818c2b0ee9da6
Anastasia <coca-cola23-3@mail.ru>
2018-11-19 02:22:06 +0300

    Selenium WebDriver

--- Lab 5 - Selenium WebDriver/SeleniumWebDriver/Test.cs
+++ Lab 5 - Selenium WebDriver/SeleniumWebDriver/Test.cs
@@ -1,19 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace Selenium_WebDriver
+namespace SeleniumWebDriver
 {
-    class Program
+    [TestClass]
+    public class Test
     {
-        static void Main(string[] args)
+        [TestMethod]
+        public void TestMethod()
         {
+            bool actual;
+
             try
             {
                 IWebDriver driver = new ChromeDriver();
                 driver.Url = "https://www.aviaru.de/ru/";
-                
+
                 var urlForPanelElement = driver.FindElement(By.Name("ypsnet-ibe-iframe"));
 
                 driver.Url = urlForPanelElement.GetAttribute("src");
@@ -21,12 +25,16 @@ namespace Selenium_WebDriver
                 driver.FindElement(By.XPath(@"//*[@id='depapt1_name']")).SendKeys("Минск");
                 driver.FindElement(By.XPath(@"//*[@id='dstapt1_name']")).SendKeys("Москва");
                 driver.FindElement(By.XPath("//*[@id='depdate_add_day']/span")).Click();
-                driver.FindElement(By.XPath(@"//*[@id='search_btn']")).Click();                
+                driver.FindElement(By.XPath(@"//*[@id='search_btn']")).Click();
+
+                actual = true;
             }
-            catch (NoSuchElementException exception)
+            catch (Exception)
             {
-                Console.WriteLine(exception.Message);
+                actual = false;
             }
+
+            Assert.AreEqual(true, actual);
         }
     }
 }
