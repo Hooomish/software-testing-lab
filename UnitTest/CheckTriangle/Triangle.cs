a1e6faebe3c31854e324d0e490db4a7f911618b4
Anastasia <coca-cola23-3@mail.ru>
2018-11-18 13:23:36 +0300

    Lab 4 - UnitTest

---
+++ UnitTest/CheckTriangle/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTriangle
+{
+    public class Triangle
+    {
+        public static bool CheckTriangle(double firstSide, double secondSide, double thirdSide)
+        {
+            double[] sides = new double[3];
+
+            sides[0] = firstSide;
+            sides[1] = secondSide;
+            sides[2] = thirdSide;
+
+            if (sides != null)
+                Array.Sort(sides);
+
+            if (sides != null
+                && sides[0] != sides[2]
+                && sides[1] != sides[2]
+                && sides[0] != 0
+                && sides[1] != 0
+                && sides[2] != 0
+                && (sides[0] + sides[1] == sides[2]))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ UnitTest/UnitTestProject/UnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using CheckTriangle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTriangle
+{
+    [TestClass]
+    public class UnitTestTriangle
+    {
+        [TestMethod]
+        public void TestTriangle1()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(5, 7, 2);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle2()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(0, 2, 2);
+            Assert.AreEqual(false, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle3()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle('s', 8, 1);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle4()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(0, 0, 0);
+            Assert.AreEqual(false, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle5()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(double.MaxValue, 7, 2);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle6()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(5, int.MaxValue, 2);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle7()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(-5, 7, -2);
+            Assert.AreEqual(false, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle8()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(1, 1, 1);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle9()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(1, 0, 1);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+
+        [TestMethod]
+        public void TestTriangle10()
+        {
+            bool isExistTriangle = Triangle.CheckTriangle(200, 500, 300);
+            Assert.AreEqual(true, isExistTriangle);
+        }
+    }
+}
